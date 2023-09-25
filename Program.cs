61eb6bed66b20a2a7ddf15257e624ac52882c25d
sukreshmanda <mandasukresh@gmail.com>
2023-09-25 19:05:27 +0530

    1. Two sum problem

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+// 1. Two Sum
+var solution = new Solution();
+Debug.Assert(solution.TwoSum([2,7,11,15], 9).SequenceEqual([0, 1]));
+Debug.Assert(solution.TwoSum([3,2,4], 6).SequenceEqual([1, 2]));
+Debug.Assert(solution.TwoSum([3,3], 6).SequenceEqual([0, 1]));
